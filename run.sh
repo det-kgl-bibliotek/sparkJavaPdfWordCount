82aee1b925ae43069d1166b0671052440acacafe
Asger Askov Blekinge <abr@kb.dk>
2018-07-24 16:01:01 +0200

    run script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+mvn clean package -Phortonworks -Psbforge-nexus
+
+scp target/spark-pdf-example-1.0-SNAPSHOT.jar $USER@kac-proj-000:.
+
+ssh $USER@kac-proj-000 <<EOF
+export SPARK_MAJOR_VERSION=2 #Or set this in your .bashrc
+spark-submit --master=yarn --class dk.kb.JavaWordCount spark-pdf-example-1.0-SNAPSHOT.jar /user/\$USER/pdfs
+EOF
