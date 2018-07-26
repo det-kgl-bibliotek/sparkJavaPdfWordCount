8961213c6300a89829ad96b51aaef82cf8168606
Asger Askov Blekinge <abr@kb.dk>
2018-07-26 15:57:08 +0200

    run-local.sh based on mvn exec plugin and works :)

--- run-local.sh
+++ run-local.sh
@@ -2,23 +2,12 @@
 set -e
 set -x
 
-mvn package
-
-#Get a version of spark
-wget -N "https://archive.apache.org/dist/spark/spark-2.1.1/spark-2.1.1-bin-hadoop2.7.tgz"
-
-#Extract
-tar -xvzf spark-2.1.1-bin-hadoop2.7.tgz --keep-newer-files
-
-#Start the local master
-spark-2.1.1-bin-hadoop2.7/sbin/start-master.sh
+#mvn package
 
 #Submit the job
-spark-2.1.1-bin-hadoop2.7/bin/spark-submit \
-    --master local \
-    --class dk.kb.JavaWordCount \
-    target/spark-pdf-example-1.0-SNAPSHOT.jar \
-    "$HOME/Downloads/*.pdf"
-
-#Stop the master
-spark-2.1.1-bin-hadoop2.7/sbin/stop-master.sh
+mvn exec:java \
+    -Dexec.classpathScope="compile" \
+    -Dexec.cleanupDaemonThreads="false" \
+    -Dexec.mainClass="dk.kb.JavaWordCount" \
+    -Dspark.master=local \
+    -Dexec.args="$HOME/Downloads/*.pdf"
