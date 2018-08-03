4ec13ed5be531498d4241c284de829084ed2eb4f
Asger Askov Blekinge <abr@kb.dk>
2018-08-03 10:33:16 +0200

    More docs for run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,8 @@ mvn clean package
 scp target/spark-pdf-example-*.jar $USER@kac-proj-000:.
 
 # -t is so that if we Ctrl-C this operation, the java process is not kept running on the remote server
+# --deploy-mode client so that if the java process on the remote server dies, the entire job dies. Otherwise the Ctrl-C
+# might not kill the job
 ssh -t $USER@kac-proj-000 env SPARK_MAJOR_VERSION=2 spark-submit \
     --master=yarn \
     --deploy-mode client \
