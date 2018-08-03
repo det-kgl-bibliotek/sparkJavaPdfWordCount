a1cd3e419f2ef35a2d442235bd17bef5769b835a
Asger Askov Blekinge <abr@kb.dk>
2018-08-03 10:28:01 +0200

    run.sh allows you to Ctrl-C the job

--- run.sh
+++ run.sh
@@ -2,16 +2,18 @@
 set -e
 set -x
 
+SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE[0]))
+
+cd $SCRIPT_DIR
+
 mvn clean package
 
-scp target/spark-pdf-example-1.0-SNAPSHOT.jar $USER@kac-proj-000:.
+scp target/spark-pdf-example-*.jar $USER@kac-proj-000:.
 
-ssh $USER@kac-proj-000 <<EOF
-export SPARK_MAJOR_VERSION=2 #Or set this in your .bashrc
-spark-submit \
+# -t is so that if we Ctrl-C this operation, the java process is not kept running on the remote server
+ssh -t $USER@kac-proj-000 env SPARK_MAJOR_VERSION=2 spark-submit \
     --master=yarn \
     --deploy-mode client \
     --class dk.kb.JavaWordCount \
-    spark-pdf-example-1.0-SNAPSHOT.jar \
+    spark-pdf-example-*.jar \
     /user/\$USER/pdfs
-EOF
